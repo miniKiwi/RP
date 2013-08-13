afb539f47edfc3ef35e8d0cd11a22ce6e0f09eca
Zalahari <Zalaharih@gmail.com>
2013-08-14 00:01:03 +0200

    Added tos.php

---
+++ tos.php
@@ -0,0 +1,30 @@
+<!--		<ul>
+		    <li><a href="#">Skriv</a></li>
+			<li><a href="#">Här finns länkar som hör ihop med sidan som spelaren är inne på.</a></li>
+			<li><a href="#">På ett rollspel skulle länkarna här vara skriv, joina (om spelaren inte är med i rollspelet) alternativt lämna (om spelaren är med och vill gå ur).</a></li>
+			<br>
+			<li><a href="#">Lämna / Joina</a></li>
+		</ul>
+	</div>
+-->
+    <div id="mainContent">
+	    <section>
+	        <p>
+			<h1>Terms of service</h1><br>
+			Do not use artwork, writing or other content that is not yours without permission. Doing so will get you a warning.<br>
+            No more than one account per person.<br>
+            Do not use any scripts or programs to "cheat" the site.<br>
+            Do not spam.<br>
+            Anything you buy with real money, even accidentally, on this site are non-refundable.<br>
+            (behålla denna?) You may not share accounts.<br>
+            Do not impersonate staff members.<br>
+            No offensive out of character content is permitted.<br>
+            Accounts on this site may not be traded at all.<br>
+            Follow any rules set by the SL (<- english for that?) in the roleplays you join, that do not violate any of the sites rules.<br>
+            <br><br>
+            All content on this site that is not user created is copyrighted to SÄTTINSIDANSNAMNHÄR.<br>
+            You may not use our copyrighted materials, and if you do, we have the right to DO SOMETHING. (<- text to be changed for a more accurate description.)<br>
+
+		    </p>
+	    </section>
+	</div>
