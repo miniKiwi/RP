230cbfbf8988f616b1180da7e9cb5db264881638
Zalahari <Zalaharih@gmail.com>
2013-08-15 20:58:19 +0200

    Revamped the search box for rp
    Placed it in a table to give it some structure.

---
+++ rp_search.php
@@ -0,0 +1,124 @@
+<ul><!--
+		    <li><a href="#">Skriv</a></li>
+			<li><a href="#">H岠finns l寫ar som h򲠩hop med sidan som spelaren 岠inne p宼/a></li>
+			<li><a href="#">P塥tt rollspel skulle l寫arna h岠vara skriv, joina (om spelaren inte 岠med i rollspelet) alternativt l宮a (om spelaren 岠med och vill g塵r).</a></li>
+			<br>
+			<li><a href="#">Lämna / Joina</a></li> -->
+		</ul>
+	</div>
+
+    <div id="mainContent">
+		<table>
+	    	Sökruta (denna blir minimerad när spelaren har skickat sin sökning. Den kan vecklas ut igen med en pil)<br>
+			<tr>
+				<th>
+					Namn på rollspel
+				</th>
+				<th>
+					Namn på spelledare
+				</th>
+				<th>
+					Genre
+				</th>
+				<th>
+					Låst/öppet (on/off knapp)
+				</th>
+					<!-- Antal spelare (nu/ev. max) -->
+				<th>
+					Nivå
+				</th>
+			</tr>
+			<tr>
+				<td>
+					<form action="">
+						<input type="text" name="name_rp"> 
+				</td>
+				<td>
+						<input type="text" name="name_sl">
+					</form>
+				</td>
+				<td>
+				dropwdown?
+				</td>
+				<td>
+					<div class="onoffswitch">
+						<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>
+							<label class="onoffswitch-label" for="myonoffswitch">
+								<div class="onoffswitch-inner"></div>
+								<div class="onoffswitch-switch"></div>
+							</label>
+					</div>
+				</td>
+<!--				<form action="">
+						Min antal spelare <input type="text" name="min_participants">
+						Aktiva spelare <input type="text" name="current_participants"> 
+						Max antal spelare <input type="text" name="max_participants">
+					</form>			-->
+				<td>
+					<form action="">
+						<select name="levels">
+							<option value="volvo">Novice</option>
+							<option value="saab">Saab</option>
+							<option value="fiat">Expert</option>
+							<option value="audi">Elite</option>
+						</select>
+					</form>
+				</td>
+			</tr>
+		</table>
+	
+	</div>
+	<div id="mainSecond">
+		<table class="tableSearch">
+			<tr>
+				<th>
+				Namn på rollspel
+				</th>
+				<th>
+				Namn på spelledare
+				</th>
+				<th>
+				Genre
+				</th>
+				<th>
+				Kort beskrivning
+				</th>
+				<th>
+				Nivå
+				</th> <!--
+				<th>
+				Antal spelare (nu/ev. max) 
+				</th> -->
+				<th>
+				Låst/öppet
+				</th>
+			</tr>
+				
+			<tr>
+				<td>
+				Rollspel 1
+				</td>
+				<td>
+				Spelledares namn
+				</td>
+				<td>
+				Fantasy
+				</td>
+				<td>
+				Kort beskrivning här
+				</td>
+				<td>
+				Elite
+				</td> <!--
+				<td>
+				2/10
+				</td> -->
+				<td>
+				öppet (img)
+				</td>
+			</tr>
+		</table>
+	    </section>
+	</div>
+	
+	
