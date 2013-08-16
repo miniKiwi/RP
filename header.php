13079d89bb80fe620e18728344de7052e518b037
Zalahari <Zalaharih@gmail.com>
2013-08-16 14:44:13 +0200

    Removed nav from header

--- header.php
+++ header.php
@@ -1,10 +1,2 @@
 <h1>Design</h1>
 <img src="images/header_rainforest_river960.png" border="1" />
-<nav id="nav">
-    <ul>
-	    <li><a href="index.php?location=index">home</a></li>
-		<li><a href="index.php?location=profile">profile</a></li>
-		<li><a href="index.php?location=rp_participatinglist">rollspel</a></li>
-		<li><a href="index.php?location=search">sök</a></li>
-	</ul>
-</nav>
