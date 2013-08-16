da1c2c0fc2fbc350ec944f313755a33633d288d8
Zalahari <Zalaharih@gmail.com>
2013-08-16 11:40:12 +0200

    Re-added the table
    Re-added the table to structure the search field.

--- rp_search.php
+++ rp_search.php
@@ -1,34 +1,74 @@
+<div id="sidebarMenu">
+nothing
+</div>
 <div id="mainContent">
-	    <section>
-		Sökruta (denna blir minimerad när spelaren har skickat sin sökning. Den kan vecklas ut igen med en pil)<br>
-		<form action="">
-		    Namn på rollspel <input type="text" name="name_rp"> 
-			Namn på spelledare <input type="text" name="name_sl">
-		</form>
-			Genre 
-			Låst/öppet (on/off knapp)<br>
-			<div class="onoffswitch">
+Sökruta (denna blir minimerad när spelaren har skickat sin sökning. Den kan vecklas ut igen med en pil)<br>
+	<table>
+		<tr>
+			<th>
+			Namn på rollspel
+			</th>
+			<th>
+			Namn på spelledare
+			</th>
+			<th>
+			Genre
+			</th>
+			<th>
+			Låst/öppet
+			</th>
+			<th>
+			Antal spelare (nu/ev. max)
+			</th>
+			<th>
+			Nivå
+			</th>
+		</tr>
+		<tr>
+			<td>
+				<form action="">
+				<input type="text" name="name_rp"> 
+			</td>
+			
+			<td>
+				<input type="text" name="name_sl">
+				</form>
+			</td>
+			
+			<td>
+			dropdown?
+			</td>
+			
+			<td>
+				<div class="onoffswitch">
 				<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>
 					<label class="onoffswitch-label" for="myonoffswitch">
 						<div class="onoffswitch-inner"></div>
 						<div class="onoffswitch-switch"></div>
 					</label>
-			</div>
-			Antal spelare (nu/ev. max)
-		<form action="">
-			Min antal spelare <input type="text" name="min_participants">
-		    Aktiva spelare <input type="text" name="current_participants"> 
-			Max antal spelare <input type="text" name="max_participants">
-		</form>			
-			Nivå
-			<form action="">
-				<select name="levels">
-					<option value="volvo">Novice</option>
-					<option value="saab">Saab</option>
-					<option value="fiat">Expert</option>
-					<option value="audi">Elite</option>
-				</select>
-			</form>
+				</div>
+			</td>
+			
+			<td>
+<!--				<form action="">
+				Min antal spelare <input type="text" name="min_participants">
+				Aktiva spelare <input type="text" name="current_participants"> 
+				Max antal spelare <input type="text" name="max_participants">
+				</form>	-->
+			</td>
+			
+			<td>
+				<form action="">
+					<select name="levels">
+						<option value="volvo">Novice</option>
+						<option value="saab">Saab</option>
+						<option value="fiat">Expert</option>
+						<option value="audi">Elite</option>
+					</select>
+				</form>
+			</td>
+		</tr>
+	</table>
 		<br>
 </div>
 	<div id="mainSecond">
