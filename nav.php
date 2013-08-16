2e0c0e696454acabccd0b81516f5a12d6b35d09d
Zalahari <Zalaharih@gmail.com>
2013-08-16 14:42:09 +0200

    Gave Nav it's own file
    Previously the navigation menu was in header.php, but now it has been
    moved to its own file. This was because of the trouble with the sidebar
    css.

---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav id="nav">
+    <ul>
+	    <li><a href="index.php?location=index">home</a></li>
+		<li><a href="index.php?location=profile">profile</a></li>
+		<li><a href="index.php?location=rp_participatinglist">rollspel</a></li>
+		<li><a href="index.php?location=search">sök</a></li>
+	</ul>
+</nav>
