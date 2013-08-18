132e21dea9884b3e220cb08baf178b9ea21ae8d7
Zalahari <Zalaharih@gmail.com>
2013-08-18 17:46:14 +0200

    Stuff related to creating roleplays and settings
    Forgot the rp_profile.php file. Whoops..

---
+++ rp_profile.php
@@ -0,0 +1,22 @@
+<div id="sidebarMenu">
+		<ul>
+		    <li><a href="#">Skriv</a></li>
+			<li><a href="#">-</a></li>
+			<li><a href="#">(if SL) Rollspelets inställningar</a></li>
+			<li><a href="#">Deltagarinställningar</a><li>
+			<br>
+			<li><a href="#">Joina / Lämna</a></li>
+		</ul>
+	</div>
+
+    <div id="mainContent">
+		<div id="rpInfo">
+			Ang menyn på vänster sida i sidebar. Om spelaren inte är med i rollspelet, finns bara alternativet "joina". Alternativt att de andra länkarna i menyn är gråa och ej går att klicka på. Bara SL (spelledaren) kan se länken till rollspelsinställningarna.<br>
+		</div>
+	</div>
+	<div id="mainSecond">
+			Här finns alla inläggen i rollspelet.
+			<div class="rpPost">
+				Detta är ett rollspelsinlägg. Det kan vara max 10.000 tecken långt.
+			</div>
+	</div>
